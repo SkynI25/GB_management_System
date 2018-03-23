705602de3f4c28935becde3718bbc5fd56f6f9e7
Ki Tae Park <rlxo1651@daum.net>
2018-03-23 13:03:29 +0900

    Update kitae_mysql.js

--- kitae_mysql.js
+++ kitae_mysql.js
@@ -29,9 +29,30 @@ app.locals.pretty = true;
 app.use(express.static('./'));
 app.set('views', './kitae_mysql');
 app.set('view engine', 'ejs');
-app.get('/upload', function(req, res) {
-  res.render('upload');
-});
+
+var connection;
+function handleDisconnect() {
+  connection = mysql.createConnection(conn); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.get('/location', function(req, res) {
   var sql = `
